8442a45e08e34d937c58a7739eca96d0a25c9303
Jainaveen-M <mjnnaveen12345@gmail.com>
2022-04-08 11:23:57 +0530

    fix : added socket for delete operation in home page

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const http = require('http').createServer(app)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/',(req,res)=>{
-    res.send("socket connected successfully...")
-})
-// routers
+app.get('/', (req, res) => {
+        res.send("socket connected successfully...")
+    })
+    // routers
 const buyrouter = require('./routes/buyorderRouter.js')
 app.use('/api/buyorder', buyrouter)
 const sellrouter = require('./routes/sellorderRouter.js')
@@ -30,7 +30,7 @@ app.use('/api/allorder', allorders)
 
 //Socket Logic
 const socketIO = require('socket.io')(http)
-a =[]
+a = []
 socketIO.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
@@ -38,51 +38,55 @@ socketIO.on("connection", (userSocket) => {
 })
 
 
-socketIO.on('connection',  (client)=> {
-  console.log('Connected...', client.id);
-// console.log('Connected broadcast', client.broadcast);
+socketIO.on('connection', (client) => {
+    console.log('Connected...', client.id);
+    // console.log('Connected broadcast', client.broadcast);
 
 
-//listens for new messages coming in
-  client.on('message',  (data) =>{
-    console.log("message",data);
-    a.push(data)
-    console.log(a.length)
-    socketIO.emit('message', data);
-  })
+    //listens for new messages coming in
+    client.on('message', (data) => {
+        console.log("message", data);
+        a.push(data)
+        console.log(a.length)
+        socketIO.emit('message', data);
+    })
 
 
-  //listens for new messages coming in
-  client.on('refresh',  (data) =>{
-    console.log("refresh",data);
-    socketIO.emit('refresh', data);
-  })
+    //listens for new messages coming in
+    client.on('refresh', (data) => {
+        console.log("refresh", data);
+        socketIO.emit('refresh', data);
+    })
+
+    client.on('delete', (data) => {
+        console.log("delete", data);
+        socketIO.emit('delete', data);
+    })
 
 
 
+    //listens when a user is disconnected from the server
+    client.on('disconnect', () => {
+        console.log('Disconnected......', client.id);
+    })
 
-//listens when a user is disconnected from the server
-  client.on('disconnect',  () =>{
-    console.log('Disconnected......', client.id);
-  })
-
-//listens when there's an error detected and logs the error on the console
-  client.on('error',  (err) =>{
-    console.log('Error detected', client.id);
-    console.log(err);
-  })
-
-
-  client.on("buyorder",(data)=>{
-    console.log("buy order...",data)
-    socketIO.emit('buyorder', data);
-  })
+    //listens when there's an error detected and logs the error on the console
+    client.on('error', (err) => {
+        console.log('Error detected', client.id);
+        console.log(err);
+    })
 
 
-  client.on("sellorder",(data)=>{
-    console.log("sell order...",data)
-    socketIO.emit('sellorder', data);
-  })
+    client.on("buyorder", (data) => {
+        console.log("buy order...", data)
+        socketIO.emit('buyorder', data);
+    })
+
+
+    client.on("sellorder", (data) => {
+        console.log("sell order...", data)
+        socketIO.emit('sellorder', data);
+    })
 
 
 })
@@ -90,7 +94,7 @@ socketIO.on('connection',  (client)=> {
 
 
 var port = process.env.PORT || 3344;
-http.listen(port, function (err) {
-  if (err) console.log(err);
-  console.log('Listening on port', port);
+http.listen(port, function(err) {
+    if (err) console.log(err);
+    console.log('Listening on port', port);
 });
