1165fe2f456b83c7bdd1ed52ac5e3980735a777b
Jainaveen-M <mjnnaveen12345@gmail.com>
2022-04-08 14:49:33 +0530

    fix : added double in sell order

--- models/sell.js
+++ models/sell.js
@@ -1,39 +1,37 @@
-
-module.exports = (sequelize,DataTypes) =>{
+module.exports = (sequelize, DataTypes) => {
     const Sell = sequelize.define(
-        "sell",
-        {
+        "sell", {
             id: {
-                type : DataTypes.INTEGER,
-                allowNull:false,
+                type: DataTypes.INTEGER,
+                allowNull: false,
                 primaryKey: true,
                 autoIncrement: true,
-                validate:{
-                    notEmpty:true,
+                validate: {
+                    notEmpty: true,
                 }
             },
             qty: {
-                type : DataTypes.INTEGER,
-                allowNull:false,
-                validate:{
-                    notEmpty:true,
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
                 }
             },
             price: {
-                type : DataTypes.INTEGER,
-                allowNull:false,
-                validate:{
-                    notEmpty:true,
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
                 }
             },
             total_price: {
-                type : DataTypes.INTEGER,
-                allowNull:false,
-                validate:{
-                    notEmpty:true,
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
                 }
             }
         }
-    ); 
+    );
     return Sell;
 };
